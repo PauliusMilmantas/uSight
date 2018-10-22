a73f5efd025150d52cee2f6332b34f20bc2e6c7c
PauliusMilmantas <pauliusmilmantas@gmail.com>
2018-10-23 01:13:18 +0300

    MySql wanted_list downloa/upload functions

--- uSight/DBConnect.cs
+++ uSight/DBConnect.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -59,5 +60,62 @@ namespace uSight
                 return false;
             }
         }
+
+        //Update DB
+        public void updateWantedListDB() {
+            if (connection != null)
+            {
+                DataExtraction e = new DataExtraction();
+
+                dynamic obj = e.GetJsonFromDisk();
+
+                MySqlCommand myCommand = new MySqlCommand("TRUNCATE TABLE wanted_list", connection);
+
+                myCommand.ExecuteNonQuery();
+
+                foreach (var json in obj.plates)
+                {                 
+                    myCommand = new MySqlCommand("INSERT INTO wanted_list (id, owner, license_plate, engine_number, vin) VALUES ('" + (String)json.id + "','" + (String)json.owner + "','" + (String)json.license_plate + "','" + (String)json.engine_number + "','" + (String)json.vin + "')", connection);
+                    
+                    myCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        struct vehicles {
+            string id;
+            string plate_number;
+            string vin;
+            string owner;
+        };
+
+        public void updateWantedListJSON() {
+            if (connection != null) {
+                DataExtraction e = new DataExtraction();
+
+                MySqlCommand myCommand = new MySqlCommand("SELECT * FROM wanted_list", connection);
+
+                MySqlDataReader dataReader = myCommand.ExecuteReader();
+
+                List<vehicles> vehicles;
+
+                e.writeToJson("{\"plates\":[]}");
+                dynamic json = e.GetJsonFromDisk();
+
+                while (dataReader.Read()) {
+                    JObject record = new JObject();
+
+                    record["id"] = (String)dataReader["id"];
+                    record["owner"] = (String)dataReader["owner"];
+                    record["vin"] = (String)dataReader["vin"];
+                    record["plate_number"] = (String)dataReader["license_plate"];
+
+                    json.plates.Add(record);
+                }
+
+                e.writeToJson(json.ToString());
+            }
+        }
+
     }
 }
