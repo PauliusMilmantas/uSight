310001e5bf6eec36feada04fd7bf5da9edde7848
PauliusMilmantas <pauliusmilmantas@gmail.com>
2018-11-04 19:09:52 +0200

    Unit test

--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -29,5 +29,42 @@ namespace UnitTests
             fwl.RemoveRecord(fwl.GetRecordCount());
         }
 
+        [TestMethod]
+        public void DBConnectionTest() {
+            uSight.DataExtraction de = new uSight.DataExtraction();
+
+            string owner = "own";
+            string plate = "TES123";
+            string id = "1";
+            string engine_number = "asdg";
+            string vin = "sdfgrth";
+
+            dynamic json = de.GetJsonFromDisk();
+            string backup = json.toString();
+
+            int i = 0;
+            foreach (dynamic t3 in json.plates) {
+                json.plates.remove(0);
+                i++;
+            }
+
+            de.writeToJson(json); //Empty json
+
+            uSight.PlateRecord t = new uSight.PlateRecord(owner, id, plate, engine_number, vin);
+            t.writeToJson();    //Local json file with 1 record
+
+            uSight.DBConnect db = new uSight.DBConnect();
+            db.updateWantedListDB(); //DB contains 1 record
+
+            db.updateWantedListJSON();
+            dynamic nde = de.GetJsonFromDisk();
+
+            Assert.AreEqual((String) nde.plates.owner + (String) nde.plates.license_plate, owner + plate);
+
+            de.writeToJson(backup); //Local JSON original v.
+            db.updateWantedListDB();
+
+            db.CloseConnection();
+        }
     }
 }
