263e2fc561b045fe60b34a4d340eb43eb5ee41b4
PauliusMilmantas <pauliusmilmantas@gmail.com>
2018-11-05 23:23:42 +0200

    new Test

--- uSightTests/Class1Tests.cs
+++ uSightTests/Class1Tests.cs
@@ -12,25 +12,67 @@ namespace uSight.Tests
     public class Class1Tests
     {
         [TestMethod()]
-        public void DBFunctionsTest()
+        public void DBFunctionsTest() //Downloading
         {
-            DataExtraction de = new DataExtraction();
-            
-            uSight.DBConnect db = new uSight.DBConnect();
-
-            dynamic old = de.GetJsonFromDisk();
+            DataExtraction de = new DataExtraction();            
+            DBConnect db = new DBConnect();
 
             db.updateWantedListJSON();
 
+            dynamic old = de.GetJsonFromDisk();
+
+            string owner = "ow2n";
+            string plate = "TES465";
+            string id = "1";
+            string engine_number = "asdg";
+            string vin = "sdfgrth";
+
+            PlateRecord pr = new PlateRecord(owner, id, plate, engine_number, vin);
+            pr.writeToJson();
+
+            dynamic mod = de.GetJsonFromDisk();
+
+            DBConnect t = new DBConnect();
+            t.updateWantedListJSON();
+
             dynamic newJson = de.GetJsonFromDisk();
-            
-            Assert.AreNotEqual(old, newJson);  
+
+            Assert.AreNotEqual(mod, newJson);  
         }
+
+        /*[TestMethod()]
+        public void testingConnection() { //Updating database
+            string owner = "ow2n";
+            string plate = "TES465";
+            string id = "1";
+            string engine_number = "asdg";
+            string vin = "sdfgrth";
+
+            DBConnect db = new DBConnect();
+            DataExtraction de = new DataExtraction();
+
+            db.updateWantedListJSON();
+
+            dynamic jsonOld = de.GetJsonFromDisk();            
+
+            PlateRecord pr = new PlateRecord(owner, id, plate, engine_number, vin);
+            pr.writeToJson();
+
+            db.updateWantedListDB();
+            db.updateWantedListJSON();
+
+            dynamic jsonNew = de.GetJsonFromDisk();
+
+            de.writeToJson(jsonOld.ToString());
+            db.updateWantedListDB();
+
+            Assert.AreNotEqual(jsonOld.ToString(), jsonNew.ToString());
+        }*/
 
         [TestMethod()]
         public void FileTest() {
 
-            string owner = "own";
+            string owner = "ow2n";
             string plate = "TES123";
             string id = "1";
             string engine_number = "asdg";
