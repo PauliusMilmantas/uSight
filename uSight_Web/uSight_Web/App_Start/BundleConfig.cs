08c2ba9c653cbc109986a9d1e46eb30a844f0c93
Arnas Kelpša <arnastk@gmail.com>
2018-11-13 00:58:55 +0200

    Commit "Hotfix" from Recognition

--- uSight_Web/uSight_Web/App_Start/BundleConfig.cs
+++ uSight_Web/uSight_Web/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace uSight_Web
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-flatly.css",
                       "~/Content/site.css"));
         }
     }
