5e2fb623d213223849f5643134467dd8cc2bf0a6
PauliusMilmantas <pauliusmilmantas@gmail.com>
2018-11-13 02:00:41 +0200

    List views

---
+++ uSight_Web/uSight_Web/Controllers/DataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace uSight_Web.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Data
+        public ActionResult Records()
+        {
+            return View();
+        }
+
+        public ActionResult AddRecord()
+        {
+            return View();
+        }
+
+        public ActionResult ViewRecords()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteRecord()
+        {
+            return View();
+        }
+    }
+}
