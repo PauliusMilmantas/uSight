65ddacef86b4669d252f4ea46c1d1d7e4b661f23
PauliusMilmantas <pauliusmilmantas@gmail.com>
2018-11-18 00:16:22 +0200

    Merge fix 2

--- uSight_Web/uSight_Web/Models/SearchRecord.cs
+++ uSight_Web/uSight_Web/Models/SearchRecord.cs
@@ -9,14 +9,22 @@
 
 namespace uSight_Web.Models
 {
-    using System;
-    using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
     
-    public partial class SearchRecord
+    public class SearchRecord
     {
-        public System.DateTime Time { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public DateTime Time { get; set; }
+        [Key]
+        [Column(Order = 2)]
         public string PlateNumber { get; set; }
-        public bool Stolen { get; set; }
-        public string UserId { get; set; }
+        public bool Stolen { get; set; } 
+		public string UserId { get; set; }
     }
 }
