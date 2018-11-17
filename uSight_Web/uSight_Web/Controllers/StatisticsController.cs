f69d79dc16dc012f1dd6ef135f3d8c194da9cb62
maciulisleonardas <43297196+maciulisleonardas@users.noreply.github.com>
2018-11-17 19:02:17 +0200

    Record relocation
    Records relocated to ApplicationDbContext, added test data and a basic chart.

--- uSight_Web/uSight_Web/Controllers/StatisticsController.cs
+++ uSight_Web/uSight_Web/Controllers/StatisticsController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using uSight_Web.Entities;
 using uSight_Web.Models;
 
 namespace uSight_Web.Controllers
@@ -12,6 +14,32 @@ namespace uSight_Web.Controllers
         // GET: Statistics
         public ActionResult Index()
         {
+            ApplicationDbContext dbc = ApplicationDbContext.Create();
+
+            ChartData timeCountSearchesCD;
+            {
+                var allQuery =
+                    from sr in dbc.SearchRecords
+                    group sr by DbFunctions.TruncateTime(sr.Time) into g
+                    orderby g.Key
+                    select new { date = g.Key, count = g.Count() };
+
+                var stolenQuery =
+                    from sr in dbc.SearchRecords
+                    group sr by DbFunctions.TruncateTime(sr.Time) into g
+                    orderby g.Key
+                    select new { date = g.Key, count = g.Count(x => x.Stolen) };
+                
+                var labels = allQuery.ToList().Select(x => x.date.HasValue ? x.date.Value.ToShortDateString() : x.ToString()).ToList();
+                var series = new List<string>() { "All", "Stolen" };
+                var data = new List<List<int>>();
+                data.Add(allQuery.Select(x => x.count).ToList());
+                data.Add(stolenQuery.Select(x => x.count).ToList());
+                timeCountSearchesCD = new ChartData(labels, series, data);
+            }
+
+            ViewBag.timeCountSearchesCD = timeCountSearchesCD;
+
             return View();
             //return HttpNotFound();
         }
---
+++ uSight_Web/uSight_Web/Models/SearchRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace uSight_Web.Models
+{
+    public class SearchRecord
+    {
+        [Key]
+        [Column(Order = 1)]
+        public DateTime Time { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public string PlateNumber { get; set; }
+        public bool Stolen { get; set; } 
+        public string UserId { get; set; }
+    }
+}
