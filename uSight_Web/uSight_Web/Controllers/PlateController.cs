2b80b9b0e9dda703bf0abf0087ca88b6c52393f5
PauliusMilmantas <pauliusmilmantas@gmail.com>
2018-12-08 17:20:00 +0200

    Removed details page

--- uSight_Web/uSight_Web/Controllers/PlateController.cs
+++ uSight_Web/uSight_Web/Controllers/PlateController.cs
@@ -13,43 +13,39 @@ namespace uSight_Web.Controllers
         // GET: Plate
         public ActionResult Index()
         {
-            ApplicationDbContext dbc = ApplicationDbContext.Create();
-
-            var list = dbc.SearchRecords.ToList();
-
-            var list2 = list.DistinctBy(x => x.PlateNumber);
-
-            return View(list2);
-        }
-
-        public ActionResult Details(String PlateNumber = "")
-        {
-            String plateNumber = PlateNumber;
-            
-            if(plateNumber.Equals("")) plateNumber = Request["plateNumber"];
-
-            ApplicationDbContext dbc = ApplicationDbContext.Create();
-
-            SearchRecord record = dbc.SearchRecords.ToList().Find(x => x.PlateNumber.Equals(plateNumber));
-
-            return View(record);
-        }
-
-        [HttpPost]
-        public ActionResult Details() {
-
             String plateNumber = Request.Form["plateNumber"];
-      
+
+            ApplicationDbContext dbc = ApplicationDbContext.Create();
+
+            if (plateNumber == null)
+            {
+                ViewData["Action"] = 0;
+
+                var list = dbc.SearchRecords.ToList();
+
+                var list2 = list.DistinctBy(x => x.PlateNumber);
+
+                return View(list2);
+            }
+            else {
+                ViewData["Action"] = 1;    
+                
+                SearchRecord record = new SearchRecord();
+                record.PlateNumber = plateNumber;
+
+                return View(record);
+            }           
+        }
+
+        public ActionResult Index(String PlateNumber) {
+
+            String plateNumber = PlateNumber;
+
+            if (plateNumber.Equals("")) plateNumber = Request["plateNumber"];
+
             ApplicationDbContext dbc = ApplicationDbContext.Create();
 
             SearchRecord record = dbc.SearchRecords.ToList().Find(x => x.PlateNumber.Equals(plateNumber));
-
-            
-            if (record == null)
-            {
-                record = new SearchRecord();
-                record.PlateNumber = plateNumber;
-            }
 
             return View(record);
         }
