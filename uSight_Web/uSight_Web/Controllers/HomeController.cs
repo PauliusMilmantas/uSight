a85b51ac165134dc29547f5078ef34326c78e7b8
PauliusMilmantas <pauliusmilmantas@gmail.com>
2018-12-08 14:32:51 +0200

    GetIPAddress()

--- uSight_Web/uSight_Web/Controllers/HomeController.cs
+++ uSight_Web/uSight_Web/Controllers/HomeController.cs
@@ -79,6 +79,23 @@ namespace uSight_Web.Controllers
             return View();
         }
 
+        protected string GetIPAddress()
+        {
+            System.Web.HttpContext context = System.Web.HttpContext.Current;
+            string ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                string[] addresses = ipAddress.Split(',');
+                if (addresses.Length != 0)
+                {
+                    return addresses[0];
+                }
+            }
+
+            return context.Request.ServerVariables["REMOTE_ADDR"];
+        }
+
         public ActionResult About()
         {
             return View();
