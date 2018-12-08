f403b5152c3ff0963c69504878d91a5f62171cb5
Arnas Kelpša <arnastk@gmail.com>
2018-12-09 00:08:36 +0200

    AchievementData Class Update
    Added a new method which allows to determine tier name by its number in the achievement group.

--- uSight_Web/uSight_Web/Entities/AchievementData.cs
+++ uSight_Web/uSight_Web/Entities/AchievementData.cs
@@ -7,7 +7,7 @@ namespace uSight_Web.Entities
 {
     public class AchievementData
     {
-        public int DetermineTier (string userID, string groupName, int count)
+        public int GetCurrentTier (string userID, string groupName, int count)
         {
             if (count == 0) return 0;
             else if (count < 0) return -1;
@@ -15,33 +15,30 @@ namespace uSight_Web.Entities
 
             switch (groupName)
             {
-                case "searcher by text":
+                case "text searcher":
                 {
                     int a = 25, b = 100, c = 300;
                     if (count < a) return 1;
                     else if (count >= a && count < b) return 2;
                     else if (count >= b && count < c) return 3;
-                    else if (count >= c) return 4;
-                    break;
+                    else return 4;
                 }
 
-                case "searcher by image":
+                case "image searcher":
                 {
                     int a = 10, b = 50, c = 100;
                     if (count < a) return 1;
                     else if (count >= a && count < b) return 2;
                     else if (count >= b && count < c) return 3;
-                    else if (count >= c) return 4;
-                    break;
+                    else return 4;
                 }
-                case "comment writer":
+                case "commenter":
                 {
                     int a = 10, b = 30, c = 100;
                     if (count < a) return 1;
                     else if (count >= a && count < b) return 2;
                     else if (count >= b && count < c) return 3;
-                    else if (count >= c) return 4;
-                    break;
+                    else return 4;
                 }
                 case "wanted plates finder":        // by both images and text searches
                 {
@@ -49,8 +46,7 @@ namespace uSight_Web.Entities
                     if (count < a) return 1;
                     else if (count >= a && count < b) return 2;
                     else if (count >= b && count < c) return 3;
-                    else if (count >= c) return 4;
-                    break;
+                    else return 4;
                 }
                 case "wanted images finder":
                 {
@@ -58,8 +54,7 @@ namespace uSight_Web.Entities
                     if (count < a) return 1;
                     else if (count >= a && count < b) return 2;
                     else if (count >= b && count < c) return 3;
-                    else if (count >= c) return 4;
-                    break;
+                    else return 4;
                 }
                 /*case "overall achiever":      // pridesiu jungimasi i db
                 {
@@ -70,7 +65,85 @@ namespace uSight_Web.Entities
                 default:
                     return -1;
             }
-            return -1;
+        }
+
+        public string GetTierName (string groupName, int tier)
+        {
+            if (tier < 0 || tier > 4) return "";
+            else if (tier == 0) return "Unranked";
+            else groupName = groupName.ToLower();
+
+            switch (groupName)
+            {
+                case "text searcher":
+                {
+                    switch (tier)
+                    {
+                        case 1: return "Novice Text Searcher";
+                        case 2: return "Skilled Text Searcher";
+                        case 3: return "Advanced Text Searcher";
+                        case 4: return "Expert Text Searcher";
+                        default: return "";
+                    }
+                }
+                case "image searcher":
+                {
+                    switch (tier)
+                    {
+                        case 1: return "Novice Image Searcher";
+                        case 2: return "Skilled Image Searcher";
+                        case 3: return "Advanced Image Searcher";
+                        case 4: return "Expert Image Searcher";
+                        default: return "";
+                    }
+                }
+                case "commenter":
+                {
+                    switch (tier)
+                    {
+                        case 1: return "Newbie Commenter";
+                        case 2: return "Intermediate Commenter";
+                        case 3: return "Experienced Commenter";
+                        case 4: return "Senior Commenter";
+                        default: return "";
+                    }
+                }
+                case "wanted plates finder":        // by both images and text searches
+                {
+                    switch (tier)
+                    {
+                        case 1: return "Rookie Spotter";
+                        case 2: return "Skilled Spotter";
+                        case 3: return "Expert Spotter";
+                        case 4: return "Phenomenal Spotter";
+                        default: return "";
+                    }
+                }
+                case "wanted images finder":
+                {
+                    switch (tier)
+                    {
+                        case 1: return "Newbie Tracker";
+                        case 2: return "Hobbyist";
+                        case 3: return "Hunter";
+                        case 4: return "Eagle Eye";
+                        default: return "";
+                    }
+                }
+                case "overall achiever":
+                {
+                    switch (tier)
+                    {
+                        case 1: return "Rookie";
+                        case 2: return "Experienced";
+                        case 3: return "Tryhard";
+                        case 4: return "uSight Senior";
+                        default: return "";
+                    }
+                }
+                default:
+                    return "";
+            }
         }
 
     }
