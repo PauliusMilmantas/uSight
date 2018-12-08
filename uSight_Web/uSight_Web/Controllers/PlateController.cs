f05647eb17b07011a090ce07fb6d5d466390227f
PauliusMilmantas <pauliusmilmantas@gmail.com>
2018-12-08 17:10:03 +0200

    Basic number list

---
+++ uSight_Web/uSight_Web/Controllers/PlateController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using uSight_Web.Models;
+
+namespace uSight_Web.Controllers
+{
+    public class PlateController : Controller
+    {
+        // GET: Plate
+        public ActionResult Index()
+        {
+            ApplicationDbContext dbc = ApplicationDbContext.Create();
+
+            var list = dbc.SearchRecords.ToList();
+
+            var list2 = list.DistinctBy(x => x.PlateNumber);
+
+            return View(list2);
+        }
+
+        public ActionResult Details(String PlateNumber = "")
+        {
+            String plateNumber = PlateNumber;
+            
+            if(plateNumber.Equals("")) plateNumber = Request["plateNumber"];
+
+            ApplicationDbContext dbc = ApplicationDbContext.Create();
+
+            SearchRecord record = dbc.SearchRecords.ToList().Find(x => x.PlateNumber.Equals(plateNumber));
+
+            return View(record);
+        }
+
+        [HttpPost]
+        public ActionResult Details() {
+
+            String plateNumber = Request.Form["plateNumber"];
+      
+            ApplicationDbContext dbc = ApplicationDbContext.Create();
+
+            SearchRecord record = dbc.SearchRecords.ToList().Find(x => x.PlateNumber.Equals(plateNumber));
+
+            
+            if (record == null)
+            {
+                record = new SearchRecord();
+                record.PlateNumber = plateNumber;
+            }
+
+            return View(record);
+        }
+    }
+}
